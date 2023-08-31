76b605f1a44f752ad1c17d1ffcb17fa19e6977ac
Jinyi Zhou <jinyizhou21@gmail.com>
2023-08-30 20:57:58 -0600

    new commit

--- HelloWorldAndroidApp/app/src/main/java/com/example/helloworldandroidapp/MainActivity.kt
+++ HelloWorldAndroidApp/app/src/main/java/com/example/helloworldandroidapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.helloworldandroidapp
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
@@ -10,12 +11,22 @@ import android.view.View
 
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         findViewById<Button>(R.id.button).setOnClickListener(){
             findViewById<TextView>(R.id.text1).text = "GitHub: xxsyang"
+
+            val textV = findViewById<TextView>(R.id.text1)
+
+            textV.setTextColor(Color.parseColor( "#FF0000"))
+
+        }
+
+        findViewById<Button>(R.id.button2).setOnClickListener(){
+            findViewById<TextView>(R.id.text1).text = "GitHub: jinyizh"
 
             val textV = findViewById<TextView>(R.id.text1)
 
