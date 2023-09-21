c077792aca364c4bafcd1e19f4c1d56f2c03ce2a
xxsyang <111765058+xxsyang@users.noreply.github.com>
2023-09-20 18:04:03 -0600

    everything works expect navigation on splash activity

--- cs6018_project/app/src/main/java/com/example/cs6018_project/activity/MainActivity.kt
+++ cs6018_project/app/src/main/java/com/example/cs6018_project/activity/MainActivity.kt
@@ -2,30 +2,41 @@ package com.example.cs6018_project.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
 import com.example.cs6018_project.R
 import com.example.cs6018_project.databinding.ActivityMainBinding
-import java.lang.Exception
 import com.example.cs6018_project.fragment.FirstFragment
+import com.example.cs6018_project.fragment.SavedBoardFragment
 import com.example.cs6018_project.fragment.SecondFragment
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
 
-//        try {
-//            binding.fragmentContainerView.getFragment<FirstFragment>().setButtonFunction {
-//                val secondFragment = SecondFragment()
-//                val transaction = this.supportFragmentManager.beginTransaction()
-//
-//                transaction.replace(R.id.fragmentContainerView, secondFragment, "draw_tag")
-//                transaction.addToBackStack(null)
-//                transaction.commit()
-//            }
-//        }
-//        catch (e: Exception) {
-//            e.printStackTrace()
-//        }
+        //val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        //val navController = navHostFragment.navController
+
+        //navController.navigate(R.id.action_firstFragment_to_savedBoardFragment)
+
+        /*
+        try {
+            binding.fragmentContainerView.getFragment<FirstFragment>().setButtonFunction {
+
+                // val secondFragment = SecondFragment()
+                val secondFragment = SavedBoardFragment()
+                val transaction = this.supportFragmentManager.beginTransaction()
+
+                transaction.replace(R.id.fragmentContainerView, secondFragment, "draw_tag")
+                transaction.addToBackStack(null)
+                transaction.commit()
+            }
+        }
+        catch (e: Exception) {
+            e.printStackTrace()
+        }
+         */
 
         setContentView(binding.root)
     }
--- cs6018_project/app/src/main/java/com/example/cs6018_project/fragment/FirstFragment.kt
+++ cs6018_project/app/src/main/java/com/example/cs6018_project/fragment/FirstFragment.kt
@@ -5,22 +5,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import com.example.cs6018_project.R
+// import androidx.fragment.app.activityViewModels
 import com.example.cs6018_project.databinding.FragmentFirstBinding
 
 class FirstFragment : Fragment() {
-    private var buttonFunction : () -> Unit = {}
-
-    fun setButtonFunction(newFunc: () -> Unit) {
-        buttonFunction = newFunc
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        // Inflate the layout for this fragment
         val binding = FragmentFirstBinding.inflate(inflater, container, false)
+
         binding.bt1.setOnClickListener {
-            buttonFunction()
+            val navController = findNavController()
+            navController.navigate(R.id.action_firstFragment_to_savedBoardFragment)
         }
 
         return binding.root
